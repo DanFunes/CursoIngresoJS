f141af56c6bc87215320909f37899a6688ec2a5f
danfunes <funes_dante@hotmail.com>
2022-02-02 22:30:55 -0300

    Nuevo cambio

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,54 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	var numerouno;
+	var numerodos;
+	var suma;
+
+
+	numerouno = document.getElementById("txtIdNumeroUno").value;
+
+	numerouno = parseInt(numerouno)
+
+	numerodos = document.getElementById("txtIdNumeroDos").value;
+
+	numerodos = parseInt(numerodos);
+
+	suma = numerouno + numerodos;
+
+	alert("La suma es " + suma);
 }
 
 function restar()
 {
 	
+	var resta;
+
+	numerouno = document.getElementById("txtIdNumeroUno").value;
+
+	numerodos = document.getElementById("txtIdNumeroDos").value;
+
+	resta = numerouno - numerodos;
+
+	alert("La resta es " + resta);
 }
 
 function multiplicar()
 { 
-	
+	var multiplicar;
+
+	numerouno = document.getElementById("txtIdNumeroUno").value;
+
+	numerodos = document.getElementById("txtIdNumeroDos").value;
+
+	multiplicar = numerouno * numerodos;
+
+	alert("La multiplicacion es " + multiplicar);
 }
 
 function dividir()
 {
+	
 	
 }
 
