f21d36a9d8ad1424682b21da34f253ea6146be5e
danfunes <funes_dante@hotmail.com>
2022-02-12 22:49:41 -0300

    nuevo cambio

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,20 +1,88 @@
 function mostrar()
 {
 	var edad;
-	var estadoCivil;
+	var nombre;
 	var mensaje;
+	var estadoCivil;
 
-	mensaje = "Es muy pequeño para NO ser soltero";
-	
-	estadoCivil = document.getElementById("estadoCivil").value;
-
+	nombre = prompt("Decime tu nombre", "");
 	edad = document.getElementById("txtIdEdad").value;
-	edad = parseFloat(edad);
+	edad = parseInt(edad);
+	estadoCivil = document.getElementById("estadoCivil").value;
+	
+	
+	if(edad < 13)
+	{
+		mensaje = "Feliz dia";
+		
+		if(nombre == "Ricardo")
+		{
+			mensaje = mensaje + ", muy chiquito para ese nombre";
+		}
+	}
+	
+	else 
+	{
+		mensaje= "Usted es adolecente";
 
-	if(edad < 18 && estadoCivil != "Soltero"){
+		if (estadoCivil == "Divorciado")
+		{
+			mensaje = mensaje + ", toda una vida por delante";
+		}
 
-		alert(mensaje);
+		if(nombre == "Violeta")
+		{
+			mensaje = mensaje + ", como un color";
+		}
+
+		if (edad == 17)
+		{
+			mensaje = mensaje + ", Ultimo año";
+		}
 
 	}
 
-}//FIN DE LA FUNCIÓN
+	if (edad > 17)
+	{
+		mensaje = "Tienes edad de laburar";
+
+		if(estadoCivil == "Soltero" && edad < 61)
+		{
+			mensaje = mensaje + ", a salir";
+		}
+
+		if (edad == 33)
+		{
+			mensaje = mensaje + ", Como cristo";
+		}
+		if (edad > 60 && edad != 88 )
+		{
+			mensaje = mensaje + ", A jubilarse";
+			
+			if(nombre == "Alfredo")
+			{
+				mensaje = mensaje + ", como el de QUEEN";
+			}
+		}
+		if (edad == 88)
+		{
+			mensaje = mensaje + ", Lindo numero";
+		}
+		 
+	}
+
+	if (edad % 2 === 0)
+	{
+		mensaje = mensaje + ", sos par!!";
+	}
+
+	if (edad > 13 && estadoCivil == "Casado")
+	{
+		mensaje = mensaje + ", casada quiere casa";
+	}
+	
+	alert(mensaje);
+
+}
+
+//FIN DE LA FUNCIÓN
